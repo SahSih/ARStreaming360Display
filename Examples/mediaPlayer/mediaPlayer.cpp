ebd8758634262f2b50213f86036ed40873281ba4
James <jmedel@hortonworks.com>
2017-06-07 12:17:18 -0700

    update mediaPlayer.cpp
    added more comments to help users understand the code

--- Examples/mediaPlayer/mediaPlayer.cpp
+++ Examples/mediaPlayer/mediaPlayer.cpp
@@ -6,21 +6,42 @@
 using namespace std;
 using namespace cv;
 
-// global variables
+// global variable keep trackbar slider position state
 int g_slider_position = 0;
-int g_run = 1, g_dontset = 0; //start out in single step mode
+/** displays new frames when g_run != 0
+* pos indicates num of frames displayed before stopping
+* neg means system runs in continuous video mode
+* when g_run = 1 and  user clicks on trackbar, 
+*  the video is paused in new location (single-step state)
+*/
+int g_run = 1;
+/**
+* Allows us to update trackbar's position
+* no more prgrammatic calls trigger single-step mode 
+* when g_dontset = 1
+*/
+g_dontset = 0; // start out in single step mode
+// global variable so callback has access to capture object
 VideoCapture g_cap;
 
-// allows user to advance video playback to new position
+/**
+* callback advances video plackback when user slides trackbar
+* @param pos 32 bit int is new trackbar position
+* @param pointer to function
+* @return void
+*/
 void onTrackbarSlide(int pos, void*)
 {
+	// advance video playback to new position
+	//// set read position in units of frames
 	g_cap.set(cv::CAP_PROP_POS_FRAMES, pos);
 
-  // After next new frame comes in, set program into single-step mode 
-  // only if callback triggered by user click
+	// after next new frame comes in, program set to single-step mode
+	// only if callback triggered by click
 	if(!g_dontset){
-		g_run = 1; //pauses video when user clicks on trackbar to jump to new location
+		g_run = 1;
 	}
+	// else callback was called by main func and sets g_dontset
 
 	g_dontset = 0;
 }
@@ -33,51 +54,59 @@ int main(int argc, char** argv) {
 		cout << "Cannot open video cam" << endl;
 		return -1;
 	}
+	// g_cap.get determines num of video frames, width, height of video images
 	int frames = (int) g_cap.get(cv::CAP_PROP_FRAME_COUNT);
 	int tmpw = (int) g_cap.get(cv::CAP_PROP_FRAME_WIDTH);
 	int tmph = (int) g_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
 	cout << "Video has " << frames << "frames of dimensions(" <<  tmpw << ", " << tmph << ") ." << endl;
 
-  // Adds slider trackbar into video player to move around quickly within video
+	//create trackbar by indicating which window it should appear in
+	// label that trackbar gets tagged wtih "Position"
+	// window trackbar gets placed into "mediaPlayer"
+	// frames num of frames in vdeo used to calibrate slider trackbar
+	// &g_slider_position variable bound to the trackbar
+	// onTrackbarSlide callback updates slider's position each time the slider is moved
 	createTrackbar("Position", "mediaPlayer", &g_slider_position, frames, onTrackbarSlide);
 
 	Mat frame;
 	for(;;){
+		// if frames are being displayed
 		if(g_run != 0){
-      // read in a frame
+			// read video frame
 			g_cap >> frame;
 			if(frame.empty()) break;
 
-      // get current frame position
+			// get current position in video
 			int current_pos = (int)g_cap.get(cv::CAP_PROP_POS_FRAMES);
-      // set, so next trackbar callback doesn't shift into single-step mode
-			g_dontset = 1; 
-
-      // update position of slider trackbar displayed to user
+			// set g_dontset, so next trackbar callback doesn't trigger single-step mode
+			g_dontset = 1;
+			
+			// invoke trackbar callback to update position of slider trackbar on display
 			setTrackbarPos("Position", "mediaPlayer", current_pos);
+			// display vdeo frame
 			imshow("mediaPlayer", frame);
 
-      // decrement to stay in single-step mode 
-      //or let video run depending on prior state set by user keypress
+			// g_run dec to stay in single-step mode
+			// or lets video run depend on prior state set by user keypress
 			g_run -= 1;
 		}
 		
-    // User able to select from two modes: single step and run mode
 		char c = (char) waitKey(10);
-    // single step: jump to new location in video with trackbar and pause
-		if(c == 's') //single step
+		if(c == 's') // single-step mode
 		{
+			// allow reads of single frame
 			g_run = 1;
 			cout << "Single step, run = " << g_run << endl;
 		}
-    //run: continuous video mode
-		if(c == 'r') //run mode
+		if(c == 'r') // continuous video mode
 		{
+			// dec g_run and leaves it neg for any conceivable video size
 			g_run = -1;
 			cout << "Run mode, run = " << g_run << endl;
 		}
-		if(c == 27)
+		if(c == 27) // esc pressed
 		{
+			// terminate program
 			break;
 		}
 	}
