630fdcb4c83310e71d262b919966e5f8de58cc7b
James <jmedel@hortonworks.com>
2017-08-04 10:46:55 -0700

    update writeToMediaFile.cpp
    support 4 cameras

--- Examples/writeToMediaFile/writeToMediaFile.cpp
+++ Examples/writeToMediaFile/writeToMediaFile.cpp
@@ -2,41 +2,63 @@
 #include <iostream>
 
 int main(int argc, char* argv[]){
-    //create window called "camera1_stream"
-    cv::namedWindow("camera1_stream,", cv::WINDOW_AUTOSIZE);
 
     //Open the video camera no. 0
-    cv::VideoCapture capture(0);
+    cv::VideoCapture cap1(0); // open the video camera no. 1
+    cv::VideoCapture cap2(1); // open the video camera no. 2
+    cv::VideoCapture cap3(2); // open the video camera no. 3
+    cv::VideoCapture cap4(3); // open the video camera no. 4
 
     //get frames per second of video
-    double fps = capture.get(cv::CAP_PROP_FPS);
+    double fps = cap1.get(cv::CAP_PROP_FPS);
     cv::Size size(
-        (int)capture.get(cv::CAP_PROP_FRAME_WIDTH),
-        (int)capture.get(cv::CAP_PROP_FRAME_HEIGHT)
+        (int)cap1.get(cv::CAP_PROP_FRAME_WIDTH),
+        (int)cap1.get(cv::CAP_PROP_FRAME_HEIGHT)
     );
+	
+    // create four windows for each writer
+    namedWindow("northCamera",CV_WINDOW_AUTOSIZE);
+    namedWindow("eastCamera",CV_WINDOW_AUTOSIZE); 
+    namedWindow("southCamera",CV_WINDOW_AUTOSIZE);
+    namedWindow("westCamera",CV_WINDOW_AUTOSIZE);
 
     //initialize VideoWriter object 
-    cv::VideoWriter writer;
-    writer.open("saveLiveStream.avi", CV_FOURCC('H','2','6','4'), fps, size);
-
+    cv::VideoWriter writer1, writer2, writer3, writer4;
+    writer1.open("saveLiveStreamNorth.avi", CV_FOURCC('M','J','P','G'), fps, size);
+    writer2.open("saveLiveStreamEast.avi", CV_FOURCC('M','J','P','G'), fps, size);
+    writer3.open("saveLiveStreamSouth.avi", CV_FOURCC('M','J','P','G'), fps, size);
+    writer4.open("saveLiveStreamWest.avi", CV_FOURCC('M','J','P','G'), fps, size);
+	
     //if initialize writer fails, exit program
-    if(!writer.isOpened())
+    if(!writer1.isOpened() && !writer2.isOpened() && !writer3.isOpened() && !writer4.isOpened())
     {
 	std::cout << "ERROR: Failed to write the video" << std::endl;
 	return -1;
     }
 
-    cv::Mat bgr_frame;
+    cv::Mat frame1, frame2, frame3, frame4;
     for(;;){
 	//read a new frame from video
-        capture >> bgr_frame;
-        if(bgr_frame.empty()) break; //end if done
-        
+        cap1 >> frame1;
+	cap2 >> frame2;
+	cap3 >> frame3;
+	cap4 >> frame4;
+        if(frame1.empty()) break; //end if done
+        if(frame2.empty()) break;
+	if(frame3.empty()) break;
+	if(frame4.empty()) break;
+	    
 	//show frame in "camera1_stream" window
-        cv::imshow("camera1_stream", bgr_frame);
+        cv::imshow("northCamera", frame1);
+	cv::imshow("eastCamera", frame2);
+	cv::imshow("southCamera", frame3);
+	cv::imshow("westCamera", frame4);
 
 	//write a frame into the file
-        writer << bgr_frame;
+        writer1 << frame1;
+	writer2 << frame2;
+	writer3 << frame3;
+	writer4 << frame4;
 
 	//wait for esc key pressed, then break loop
         char c = cv::waitKey(10);
